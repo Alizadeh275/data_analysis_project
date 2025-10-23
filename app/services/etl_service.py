a555316a330447ef0ae534af5a47f09fff215753
Alizadeh275 <sajjadalizadeh275800@gamil.com>
2025-10-23 10:59:40 -0400

    added loader_service.py

--- app/services/etl_service.py
+++ app/services/etl_service.py
@@ -1,8 +1,8 @@
 import os
 from app.services.db_cleaner import clear_workorder_tables
 from app.services.excel_transformer import ExcelTransformer
-from app.utils.bulk_loader import load_work_orders_bulk_full
 from app.core.constants import WIDE_FILE_PATH, LONG_EXCEL_PATH, LONG_CSV_PATH
+from app.services.load_service import WorkOrderLoader
 
 async def run_etl(db):
     await clear_workorder_tables(db)
@@ -14,7 +14,9 @@ async def run_etl(db):
     long_df.to_excel(LONG_EXCEL_PATH, index=False)
     long_df.to_csv(LONG_CSV_PATH, index=False)
 
-    await load_work_orders_bulk_full(long_df, db)
+
+    loader = WorkOrderLoader(long_df)
+    await loader.load(db)
 
     return {
         "status": "success",
