e3389809f0210b972079837205c4bda501f23c1a
= <sajjadalizadeh275800@gmail.com>
2025-10-23 19:03:57 +0330

    renamed the endpoints

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.routers.aggregations_router import router as aggregations_router
 from app.routers.etl_router import router as etl_router
 from app.routers.dimensions_router import router as dimensions_router
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
@@ -12,12 +13,10 @@ async def lifespan(app: FastAPI):
     yield
     await engine.dispose()
 
+
 app = FastAPI(title="Open Work Orders App", lifespan=lifespan)
 
 # Register Routers
-app.include_router(etl_router, prefix="/open-workorders")
-app.include_router(dimensions_router, prefix="/open-workorders")
-app.include_router(aggregations_router,prefix="/open-workorders")
-
-
-
+app.include_router(etl_router, prefix="/open-work-orders")
+app.include_router(dimensions_router, prefix="/open-work-orders")
+app.include_router(aggregations_router, prefix="/open-work-orders")
